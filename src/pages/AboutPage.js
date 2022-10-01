9fd17006dc12d1926a096fdc68f18f7da43491ab
Sean Klein <87321511+Seanrk06@users.noreply.github.com>
2022-10-01 19:48:14 -0400

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,7 +3,6 @@ import AboutIcons from "../components/AboutIcons";
 import AboutTitle from "../components/AboutTitle";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Title from "../components/Title";
 
 function AboutPage() {
   
@@ -28,16 +27,3 @@ function AboutPage() {
 }
 
 export default AboutPage;
-
-{/* <h1
-id="Header"
-className="m-12 animate-fadein text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl"
->
-About Me
-</h1>
-<p className="m-12 text-base text-1xl text-zinc-600 ">
-My name is Sean Klein, I started to learn how to program in 2020 and
-from there on I've gone from writing rock paper sicorrers in Python to
-building websites like the one you looking at right now. Im currently
-interested
-</p> */}
