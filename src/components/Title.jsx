cebb37b38049051ea9a20b957b361dbf2695e694
Sean Klein <87321511+Seanrk06@users.noreply.github.com>
2022-10-01 18:10:08 -0400

    Updates

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title(props) {
   return (
-    <div className="grid grid-rows-1 col-2">
+    <div className="grid grid-rows-1 col-2 mt-10">
       <div className="text-center mt-8 animation-fadein">
         <h1 className="text-4xl m-5 font-bold tracking-tight text-zinc-800 sm:text-5xl text-white">
           {props.heading}
